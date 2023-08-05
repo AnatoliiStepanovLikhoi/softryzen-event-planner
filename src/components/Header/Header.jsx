289a61a4c2972261c35841dfb611210c71611d25
AnatoliiStepanovLikhoi <105781107+AnatoliiStepanovLikhoi@users.noreply.github.com>
2023-08-05 09:51:36 +0300

    fix: input and header details

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,10 +2,8 @@ import {
   HeaderBackground,
   HeaderContainer,
   Logo,
-  // Input,
   InputContainer,
   SearchIcon,
-  // HeaderTopWrapper,
   LanguageSelectContainer,
 } from "./Header.styled";
 import LanguageSelect from "../LanguageSelect/LanguageSelect";
@@ -15,13 +13,12 @@ const Header = () => {
   return (
     <HeaderBackground>
       <HeaderContainer>
-        {/* <HeaderTopWrapper> */}
         <Logo>Event Planner</Logo>
+
         <LanguageSelectContainer>
           <LanguageSelect />
         </LanguageSelectContainer>
 
-        {/* </HeaderTopWrapper> */}
         <InputContainer>
           <SearchInput placeholder="Search by keywords" />
           <SearchIcon />
